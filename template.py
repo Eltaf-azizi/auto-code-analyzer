c10d22d02f90bc4d8a672326c33c4fc767204ec0
Eltaf-azizi <iltfhuss@gmail.com>
2025-08-02 22:27:13 +0500

    working on the condition

--- template.py
+++ template.py
@@ -30,5 +30,20 @@ for filepath in list_of_files:
         os.markdirs(filedir, exit_ok=True)
         logging.info(f"Creating directory; {filedir} for the file: {filename}")
 
+
+
+    
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+
+        
+    else:
+        logging.info(f"{filename} is already exists")
+
     
     
